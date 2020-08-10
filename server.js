80223975060d36da973d1f91c6d2e251a2b7b9ed
Julian Villalba <62047235+jrvillalba1993@users.noreply.github.com>
2020-08-09 23:02:10 -0400

    project attempt

---
+++ server.js
@@ -0,0 +1,40 @@
+//Modules
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var express = require("express");
+var app = express();
+
+app.use(logger("dev"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+//Handlebar Stuffzzz
+app.use(express.static(process.cwd() + "/public"));
+var exphbs = require("express-handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+//Mongo Connection
+app.set("view engine", "handlebars");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("Connected to Mongoose!");
+});
+//Port
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
